01519bbf5bfe4673d1af2ad0bbcffbd70d544717
Lilach Vald <lilachvald@gmail.com>
2018-03-16 11:11:20 +0200

    added url ex

--- Ex12/Ex12__.py
+++ Ex12/Ex12__.py
@@ -6,3 +6,15 @@ groups = re.findall(regex, text)
 print(groups)
 output = [int(x[6:9]) for x in groups]
 print(output)
+
+
+text1 = """This http://www.foo.com/gag.txt is a web URL. This other one HTTP://WWW.UPPERCASE.COM is too.
+on the other hand httl://www.google.com/index.html and mailto:fooman@foo.com are not.
+http://spaz.info/31415.html is quite OK too. http:///www.cnn.com is messed up."""
+
+webURLregex = r"((http|HTTP)://([^/\s]+))"
+groups = re.findall(webURLregex, text1)
+print(groups)
+thirdGroup = groups[2]
+output = thirdGroup[2]
+print(output)
