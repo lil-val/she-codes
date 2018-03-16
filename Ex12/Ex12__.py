15620ecfedcc88acefa9200f4839cb0ad3aaf725
Lilach Vald <lilachvald@gmail.com>
2018-03-16 10:08:56 +0200

    added hyphen

--- Ex12/Ex12__.py
+++ Ex12/Ex12__.py
@@ -1,5 +1,5 @@
 import re
-text = "(note 1): my phone number is (541) 754-3010, yours is (555) 323-7121. I have 912 cellphones as you can see on page (414). This (071) 314-1592 is not a valid US phone number."
+text = "(note 1): my phone number is (541) 754-3010, your's is (555) 323-7121. I have 912 cellphones as you can see on page (414). This (071) 314-1592 is not a valid US phone number."
 regex = r'(\([23456789][0-9]{2}\) \d{3}-\d{4})'
 
 groups = re.findall(regex, text)
